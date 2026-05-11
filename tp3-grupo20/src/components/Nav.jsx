9b53d2cb8160709d88529acaf99fdb485f06aa9f
gisela1234 <gise201777@gmail.com>
2026-05-11 16:10:45 -0300

    agrego componente nav CarlosGisela

---
+++ tp3-grupo20/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav= ()=>{
+ return(
+    <nav>
+     <ul>
+       <li> <a href="#">INICIO</a> </li>
+       <li> <a href="#"> EXPLORADOR DE PROYECTOS</a> </li>
+       <li> <a href ="#">DETALLE DEL PROYECTO  </a> </li>
+       <li> <a href="#">PERFIL DEL USUARIO</a> </li>
+     </ul>
+   </nav>
+ )
+}
+export default Nav;

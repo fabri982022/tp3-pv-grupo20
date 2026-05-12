f80ac081be6de9b452564816d1da59d7bbcce5fe
fabri98bazan <fabri98201098@gmail.com>
2026-05-12 10:41:27 -0300

    modificacion de enlaces y archivos, Bazán Fabricio

--- tp3-grupo20/src/components/Footer.jsx
+++ tp3-grupo20/src/components/Footer.jsx
@@ -1,13 +1,7 @@
-import React from "react";
-
-const Footer=()=> {
- return(
-   <Footer>
-        <p>&copy; 2026 - Programación Visual - Grupo 20- Facultad de Ingeniería UNJU</p>
-
-   </Footer>
-
- );
-};
-
-export default Footer;
+export default function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2026 - Programación Visual - Grupo 20 - Facultad de Ingeniería UNJU</p>
+    </footer>
+  );
+}
--- tp3-grupo20/src/components/Nav.jsx
+++ tp3-grupo20/src/components/Nav.jsx
@@ -1,15 +1,12 @@
-import React from "react";
-
-const Nav= ()=>{
- return(
+export default function Nav() {
+  return (
     <nav>
-     <ul>
-       <li> <a href="#">INICIO</a> </li>
-       <li> <a href="#"> EXPLORADOR DE PROYECTOS</a> </li>
-       <li> <a href ="#">DETALLE DEL PROYECTO  </a> </li>
-       <li> <a href="#">PERFIL DEL USUARIO</a> </li>
-     </ul>
-   </nav>
- );
-};
-export default Nav;
+      <ul>
+        <li><a href="#">INICIO</a></li>
+        <li><a href="#">EXPLORADOR DE PROYECTOS</a></li>
+        <li><a href="#">DETALLE DEL PROYECTO</a></li>
+        <li><a href="#">PERFIL DEL USUARIO</a></li>
+      </ul>
+    </nav>
+  );
+}

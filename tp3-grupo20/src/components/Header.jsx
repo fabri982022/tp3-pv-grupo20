8836168c0d7a22b3bc79f5cd63928f133d6d24c4
fabri98bazan <fabri98201098@gmail.com>
2026-05-11 23:03:46 -0300

    Refactorizacion del App y Header, Bazan Fabricio

---
+++ tp3-grupo20/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="navbar">
+      <h1>Plataforma de Gestión de Proyectos Educativos - FI-UNJU</h1>
+      <p>Bienvenido usuario a la plataforma</p>
+    </header>
+  );
+}
